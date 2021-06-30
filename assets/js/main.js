d215fd05dffac1d6749398b4faed10ced256adbe
DinhMinh2810 <dinhminh28102000@gmail.com>
2021-06-30 21:43:17 +0700

    add modal search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
+const inputSearch = document.querySelector('.header__search-input-wrap');
+const historySearch = document.querySelector('.header__search--history');
+const modal = document.querySelector('.modal__overlay');
+
+inputSearch.addEventListener('click', () => {
+   historySearch.classList.toggle('show__history')
+   modal.classList.toggle('open-modal')
+})
+
+
+
+
+
 
