113159f87cc4c0e99f51146c4f8b77e592703420
DinhMinh2810 <dinhminh28102000@gmail.com>
2021-07-01 21:37:44 +0700

    Complete header menu on PC

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,11 @@
-const inputSearch = document.querySelector('.header__search-input-wrap');
-const historySearch = document.querySelector('.header__search--history');
-const modal = document.querySelector('.modal__overlay');
+const inputSearch = document.querySelector(".header__search-input-wrap");
+const historySearch = document.querySelector(".header__search--history");
+const modal = document.querySelector(".modal__overlay");
+const menuLogo = document.querySelector(".menu__logo");
+const menuProductItem = document.querySelector(".product__category--item");
 
-inputSearch.addEventListener('click', () => {
-   historySearch.classList.toggle('show__history')
-   modal.classList.toggle('open-modal')
-})
-
-
-
-
-
+inputSearch.addEventListener("click", () => {
+  historySearch.classList.toggle("show__history");
+  modal.classList.toggle("open-modal");
+});
 
