34e49fbbf3324bbe026291ffd690e70e3a0d2477
DinhMinh2810 <dinhminh28102000@gmail.com>
2021-07-03 22:37:34 +0700

    shock time coutdown and product coutdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,26 @@
 const inputSearch = document.querySelector(".header__search-input-wrap");
 const historySearch = document.querySelector(".header__search--history");
-const modal = document.querySelector(".modal__overlay");
 const slides = document.querySelectorAll(".mySlides");
 const dots = document.querySelectorAll(".dot");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
+const countDownDate = new Date("Oct 5, 2021 23:00:00").getTime();
+const timeHours = document.querySelector(".shock__time--hours");
+const timeMinutes = document.querySelector(".shock__time--minutes");
+const timeSeconds = document.querySelector(".shock__time--seconds");
+const outTime = document.querySelector(".shock__time--coutdown");
 let slideIndex = 1;
 showSlides(slideIndex);
 
 // Search function
 inputSearch.addEventListener("click", () => {
   historySearch.classList.toggle("show__history");
-  modal.classList.toggle("open-modal");
 });
 
 // Slide ad
 let plusSlides = function (n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 };
-
 
 let currentSlide = function (n) {
   showSlides((slideIndex = n));
@@ -47,16 +49,44 @@ function showSlides(n) {
 
 next.addEventListener("click", () => {
   plusSlides(1);
-})
+});
 
 prev.addEventListener("click", () => {
   plusSlides(-1);
-})
+});
 
-dots.forEach(function (dot) {
-  dot.addEventListener("click", e => {
+dots.forEach((dot) => {
+  dot.addEventListener("click", (e) => {
     const id = e.target.dataset.id;
     currentSlide(id);
   });
 });
 
+// Countdown timers
+
+function pad(d) {
+  return d < 10 ? "0" + d.toString() : d.toString();
+}
+
+const x = setInterval(() => {
+  const now = new Date().getTime();
+
+  const distance = countDownDate - now;
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  timeHours.innerHTML = pad(hours);
+  timeMinutes.innerHTML = pad(minutes);
+  timeSeconds.innerHTML = pad(seconds);
+
+  if (distance < 0) {
+    clearInterval(x);
+    outTime.innerHTML = "Out time sales !!";
+    outTime.style.fontSize = " 1.6em";
+    outTime.style.fontWeight = "600";
+    outTime.style.color = "rgb(120, 120, 120)";
+  }
+}, 0);
